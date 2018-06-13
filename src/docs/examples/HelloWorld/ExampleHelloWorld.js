2c491d446429087cb20268bf3b1c681273a64c5f
Erik <erik.secker@cognitivewave.com>
2018-06-13 14:41:32 -0400

    import alias

--- src/docs/examples/HelloWorld/ExampleHelloWorld.js
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HelloWorld from '../../../components/HelloWorld/HelloWorld';
+import HelloWorld from 'ps-react-components-cogwave/HelloWorld/HelloWorld';
 
 /** custom message */
 export default function ExampleHelloWorld() {
