37e32a4819b05c94f2183f9501c946d3e9e6c25c
Erik <erik.secker@cognitivewave.com>
2018-06-13 14:37:49 -0400

    Example for HelloWorld

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from '../../../components/HelloWorld/HelloWorld';
+
+/** custom message */
+export default function ExampleHelloWorld() {
+	return <HelloWorld message="PluralSight Viewers" />;
+}
