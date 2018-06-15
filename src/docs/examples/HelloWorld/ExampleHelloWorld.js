adc83034df091f013cb9108b6b0afca4f78d4d2b
Erik <erik.secker@cognitivewave.com>
2018-06-15 15:01:43 -0400

    using "ps-react" as import name for ease of copy-paste, definitely want to change it back

--- src/docs/examples/HelloWorld/ExampleHelloWorld.js
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HelloWorld from 'ps-react-components-cogwave/HelloWorld';
+import HelloWorld from 'ps-react/HelloWorld';
 
 /** custom message */
 export default function ExampleHelloWorld() {
