1941b44160b44175e97599c7f92310a8ae0f7260
Erik <erik.secker@cognitivewave.com>
2018-06-19 16:52:49 -0400

    components library index file

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export {default as EyeIcon} from './EyeIcon';
+export {default as HelloWorld} from './HelloWorld';
+export {default as Label} from './Label';
+export {default as ProgressBar} from './ProgressBar';
